1a38834a89c97d023caa22e86edcbd5c716b9e6c
Michael Abdulai <mabdulai90@gmail.com>
2023-02-27 14:55:10 +0100

    trigger build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
     site: 'https://mabdulai.github.io',
     base: '/justus-league',
