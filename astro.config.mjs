d656b6cc8b435603cfb76d4f905836d32944d8ff
Michael Abdulai <mabdulai90@gmail.com>
2023-03-05 14:46:28 +0100

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import react from '@astrojs/react';
+import image from "@astrojs/image";
 
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://mabdulai.github.io',
-    base: '/justus-league',
-    integrations: [mdx(), sitemap(), react()],
+  site: 'https://mabdulai.github.io',
+  base: '/justus-league',
+  integrations: [mdx(), sitemap(), react(), image()]
 });
---
+++ src/components/Character.tsx
@@ -0,0 +1,31 @@
+import styles from '../styles/Character.module.css';
+
+interface CharacterProps {
+    name: string;
+    picture: string;
+    race: string;
+    klas: string;
+    level: number;
+}
+
+const Character: React.FC<CharacterProps> = ({ name, picture, race, klas, level }) => {
+    return (
+        <div className={styles.card}>
+            <div className={styles.characterImage}>
+                <img src={picture} alt="Fernir" />
+            </div>
+
+            <div className={styles.border}>
+                <div className={styles.level}>{level}</div>
+            </div>
+            <div className={styles.info}>
+                <div className={styles.name}>{name}</div>
+                <div className={styles.desc}>
+                    {klas} {race}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Character;
